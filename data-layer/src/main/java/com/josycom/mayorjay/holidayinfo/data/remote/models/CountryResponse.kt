bc91c77eb315918f84a90e8a0bb1f69d9ab5f26d
mayorjay <joseolu4gsm@yahoo.com>
2022-10-11 20:53:15 +0100

    feat: Extract Data layer into a Module

--- data-layer/src/main/java/com/josycom/mayorjay/holidayinfo/data/remote/models/CountryResponse.kt
+++ data-layer/src/main/java/com/josycom/mayorjay/holidayinfo/data/remote/models/CountryResponse.kt
@@ -1,3 +1,5 @@
 package com.josycom.mayorjay.holidayinfo.data.remote.models
 
+import com.josycom.mayorjay.holidayinfo.data.remote.models.CountryRemote
+
 data class CountryResponse(val countries: List<CountryRemote>)
