56ca37340cde9b8ac3e8539c5fb18c29c201ad8a
mayorjay <joseolu4gsm@yahoo.com>
2022-11-12 23:01:49 +0100

    feat: Cleanups

--- app/src/main/java/com/josycom/mayorjay/holidayinfo/viewmodel/DetailsViewModel.kt
+++ app/src/main/java/com/josycom/mayorjay/holidayinfo/viewmodel/DetailsViewModel.kt
@@ -1,10 +1,12 @@
-package com.josycom.mayorjay.holidayinfo.viemodel
+package com.josycom.mayorjay.holidayinfo.viewmodel
 
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.josycom.mayorjay.holidayinfo.data.model.Country
+import com.josycom.mayorjay.holidayinfo.data.model.Holiday
 import com.josycom.mayorjay.holidayinfo.data.repository.HolidayInfoRepository
+import com.josycom.mayorjay.holidayinfo.data.remote.models.HolidayRequest
 import com.josycom.mayorjay.holidayinfo.util.UIState
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.launch
@@ -12,17 +14,26 @@ import timber.log.Timber
 import javax.inject.Inject
 
 @HiltViewModel
-class OverviewViewModel @Inject constructor(private val repository: HolidayInfoRepository) : ViewModel() {
+class DetailsViewModel @Inject constructor(private val repository: HolidayInfoRepository) : ViewModel() {
 
-    var yearSelected: String? = null
-    private val _uiState: MutableLiveData<UIState<List<Country>>> = MutableLiveData()
+    private var _country = Country()
+    var country: Country
+        get() = _country
+        set(value) { _country = value }
 
-    fun getCountries() {
+    private var _yearSelected: String = ""
+    var yearSelected: String
+        get() = _yearSelected
+        set(value) { _yearSelected = value }
+
+    private val _uiState: MutableLiveData<UIState<List<Holiday>>> = MutableLiveData()
+
+    fun getHolidays(holidayRequest: HolidayRequest) {
         viewModelScope.launch {
             _uiState.postValue(UIState.Loading)
 
-            val result = repository.getCountries()
-            val state: UIState<List<Country>> = if (result.isSuccess) {
+            val result = repository.getHolidays(holidayRequest)
+            val state: UIState<List<Holiday>> = if (result.isSuccess) {
                 UIState.Success(result.getOrDefault(emptyList()))
             } else {
                 result.exceptionOrNull()?.let {
